93d65a1ed19fa09e2cf4c313a3e4eb9e47ba9cd0
Maksym Kozlov <kozlovmaksinn@gmail.com>
2023-02-21 11:14:12 +0200

    FE-1 version 1

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+from decouple import config
+
+
+app = FastAPI()
+
+@app.get('/')
+async def health():
+    return {
+             "status_code": 200,
+             "detail": "ok",
+             "result": "working"
+           }
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host=config('host_port', cast=str), port=config('app_port', cast=int), reload=True)
