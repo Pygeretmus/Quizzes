61c4706c5bfef44f98a931bfa7b886d538139c8e
Maksym Kozlov <kozlovmaksinn@gmail.com>
2023-02-28 12:12:38 +0200

    FE-4 version 3

--- app/connections.py
+++ app/connections.py
@@ -8,7 +8,6 @@ from sqlalchemy.ext.asyncio import create_async_engine
 DATABASE_URL = f"postgresql+asyncpg://{config('POSTGRES_USER')}:{config('POSTGRES_PASSWORD')}@{config('POSTGRES_HOST')}:{config('POSTGRES_PORT')}/{config('POSTGRES_DATABASE')}" 
 db = Database(DATABASE_URL)
 redis = False
-engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 metadata = sqlalchemy.MetaData()
 
 def get_db():
