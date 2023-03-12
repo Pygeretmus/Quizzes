6b7c29d15dd6cf6665c134c108882cdf316fdd6c
Maksym Kozlov <kozlovmaksinn@gmail.com>
2023-03-12 18:56:57 +0200

    FE-7 version 1

--- app/routers/user_route.py
+++ app/routers/user_route.py
@@ -3,24 +3,27 @@ from services.user_service import UserService
 from databases import Database
 from schemas.user_schema import *
 from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
 
 
 router = APIRouter()
 
+auth_token_schema = HTTPBearer()
+
 
 @router.get('/users', response_model=UserListResponse)
-async def get_all_users(db: Database = Depends(get_db)) -> UserListResponse:
-    return await UserService(db=db).get_users()
+async def get_all_users(token: str = Depends(auth_token_schema), db: Database = Depends(get_db)) -> UserListResponse:
+    return await UserService(db=db).get_users(token=token)
  
 
 @router.get('/user', response_model=UserResponse)
-async def get_user_by_id(user_id:int, db: Database = Depends(get_db)) -> UserResponse:
-    return await UserService(db=db).get_user_id(id=user_id)
+async def get_user_by_id(user_id:int, token: str = Depends(auth_token_schema), db: Database = Depends(get_db)) -> UserResponse:
+    return await UserService(db=db).get_user_id(id=user_id, token=token)
 
 
 @router.delete('/user', response_model=str)
-async def delete_user_id(user_id: int, db: Database = Depends(get_db)) -> str:
-    return await UserService(db=db).delete_user(id=user_id)
+async def delete_user_id(user_id: int, token: str = Depends(auth_token_schema), db: Database = Depends(get_db)) -> str:
+    return await UserService(db=db).delete_user(id=user_id, token=token)
 
 
 @router.post('/user', response_model=UserResponse)
@@ -29,6 +32,7 @@ async def create_new_user(data:SignUpRequest, db: Database = Depends(get_db)) ->
 
 
 @router.put('/user', response_model=UserResponse)
-async def upgrade_user(user_id: int, data:UserUpdateRequest, db: Database = Depends(get_db)) -> UserResponse:
-    return await UserService(db=db).update_user(id=user_id, data=data)
+async def upgrade_user(user_id: int, data:UserUpdateRequest, token: str = Depends(auth_token_schema), db: Database = Depends(get_db)) -> UserResponse:
+    return await UserService(db=db).update_user(id=user_id, data=data, token=token)
+
 
--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -35,7 +35,6 @@ class SignUpRequest(SignInRequest):
 class UserUpdateRequest(BaseModel):
     user_name: Optional[str] = ""
     user_password: Optional[str] = ""
-    user_status: Optional[str] = ""
 
 
 class UserResponse(BaseModel):
@@ -44,3 +43,12 @@ class UserResponse(BaseModel):
 
 class UserListResponse(BaseModel):
     result: Userlist
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenResponse(BaseModel):
+    result: Token
