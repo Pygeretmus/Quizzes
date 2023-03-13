86fa67be2757719787a7c909f357d28f49bb9c23
Maksym Kozlov <kozlovmaksinn@gmail.com>
2023-03-13 13:12:23 +0200

    FE-7 version 2

--- app/routers/user_route.py
+++ app/routers/user_route.py
@@ -3,27 +3,25 @@ from services.user_service import UserService
 from databases import Database
 from schemas.user_schema import *
 from fastapi import APIRouter, Depends
-from fastapi.security import HTTPBearer
+from routers.auth import get_current_user
 
 
 router = APIRouter()
 
-auth_token_schema = HTTPBearer()
-
 
 @router.get('/users', response_model=UserListResponse)
-async def get_all_users(token: str = Depends(auth_token_schema), db: Database = Depends(get_db)) -> UserListResponse:
-    return await UserService(db=db).get_users(token=token)
+async def get_all_users(user: UserResponse = Depends(get_current_user), db: Database = Depends(get_db)) -> UserListResponse:
+    return await UserService(db=db).get_users()
  
 
 @router.get('/user', response_model=UserResponse)
-async def get_user_by_id(user_id:int, token: str = Depends(auth_token_schema), db: Database = Depends(get_db)) -> UserResponse:
-    return await UserService(db=db).get_user_id(id=user_id, token=token)
+async def get_user_by_id(user_id:int, user: UserResponse = Depends(get_current_user), db: Database = Depends(get_db)) -> UserResponse:
+    return await UserService(db=db).get_user_id(id=user_id)
 
 
 @router.delete('/user', response_model=str)
-async def delete_user_id(user_id: int, token: str = Depends(auth_token_schema), db: Database = Depends(get_db)) -> str:
-    return await UserService(db=db).delete_user(id=user_id, token=token)
+async def delete_user_id(user_id: int, user: UserResponse = Depends(get_current_user), db: Database = Depends(get_db)) -> str:
+    return await UserService(db=db, user=user).delete_user(id=user_id)
 
 
 @router.post('/user', response_model=UserResponse)
@@ -32,7 +30,7 @@ async def create_new_user(data:SignUpRequest, db: Database = Depends(get_db)) ->
 
 
 @router.put('/user', response_model=UserResponse)
-async def upgrade_user(user_id: int, data:UserUpdateRequest, token: str = Depends(auth_token_schema), db: Database = Depends(get_db)) -> UserResponse:
-    return await UserService(db=db).update_user(id=user_id, data=data, token=token)
+async def upgrade_user(user_id: int, data:UserUpdateRequest, user: UserResponse = Depends(get_current_user), db: Database = Depends(get_db)) -> UserResponse:
+    return await UserService(db=db, user=user).update_user(id=user_id, data=data)
 
 
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -6,15 +6,14 @@ from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
 from fastapi import HTTPException, status
 import datetime
-import random
-from core.security import create_access_token, decode_access_token
-from pydantic import ValidationError
+
 
 
 class UserService:
 
-    def __init__(self, db:Database):
+    def __init__(self, db:Database, user:UserResponse=None):
         self.db = db
+        self.user = user
 
 
     async def make_changes(self, data: UserUpdateRequest) -> UserUpdateRequest:
@@ -51,39 +50,13 @@ class UserService:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
 
 
-    async def validate(self, token: str):
-        payload = decode_access_token(token.credentials)
-        if not payload:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token is not valid")
-        try:
-            email = payload["https://example.com/email"]
-        except KeyError:
-            email = payload["sub"]
-            if not email:
-                raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Email is not valid")
-        user = await self.get_user_email(email=email)
-        if not user:
-            password = ''
-            for x in range(10):
-                password += random.choice(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'))
-            user = await self.create_user(account= SignUpRequest(
-            user_email = email, 
-            user_password = password,
-            user_password_repeat = password,
-            user_name = "User" 
-        ))
-        return user
-
-
-    async def get_users(self, token:str) -> UserListResponse:
-        await self.validate(token=token)
+    async def get_users(self) -> UserListResponse:
         query = select(Users)
         result = await self.db.fetch_all(query=query)
         return UserListResponse(result = Userlist(users = [User(**item) for item in result]))
 
 
-    async def get_user_id(self, id: int, token: str) -> UserResponse:
-        await self.validate(token=token)
+    async def get_user_id(self, id: int) -> UserResponse:
         query = select(Users).where(Users.user_id == id)
         result = await self.db.fetch_one(query=query)
         if not result:
@@ -107,18 +80,16 @@ class UserService:
         return UserResponse(result=await self.db.fetch_one(query=query))
 
 
-    async def delete_user(self, id: int, token:str) -> str:
-        user = await self.validate(token=token) 
-        if user.result.user_id != id:
+    async def delete_user(self, id: int) -> str:
+        if self.user.result.user_id != id:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="It's not your account")
         query = delete(Users).where(Users.user_id == id)
         await self.db.execute(query=query)
         return "Successfully deleted"
 
 
-    async def update_user(self, id: int, data: UserUpdateRequest, token:str) -> UserResponse:
-        user = await self.validate(token=token) 
-        if user.result.user_id != id:
+    async def update_user(self, id: int, data: UserUpdateRequest) -> UserResponse: 
+        if self.user.result.user_id != id:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="It's not your account")
         changes = await self.make_changes(data = data)
         if changes: 
